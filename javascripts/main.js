f47ad4863214e27482c6aec6420336f11ab7cba6
Lauren Johnson <laurenaudry1995@gmail.com>
2016-08-29 02:27:18 -0500

    changed it all

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,21 @@
-jQuery(document).ready(function () {
+$(function(){
 
-  jQuery('a.album').colorbox({ opacity:0.5 ,
-  rel:'group1'
+  var $imgHolderElement = $("[data-js='img-container']");
+  var $modalElement = $("[data-js='modal']");
+  var $modalImgElement = $("[data-js='modal__image']");
+  var $closeElement = $("[data-js='close-modal']");
+
+  $imgHolderElement.on("click", function(e){
+    var $imageSrc = $(e.currentTarget.children[0]).attr("src");
+    var $imageTitle = $(e.currentTarget.children[1]).text();
+    $modalElement.toggleClass("modal__visible");
+    $modalImgElement.attr("src", $imageSrc);
+  });
+
+  $closeElement.on("click", function(e){
+    $("div h2").remove();
+    $modalImgElement.attr("src", "");
+    $modalElement.toggleClass("modal__visible");
+  });
 
 });
